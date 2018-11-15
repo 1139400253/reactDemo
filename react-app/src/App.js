30174d44f077de2e5f5b2c54b8c496b8505e2c29
1139400253 <1139400253@qq.com>
2018-11-15 10:21:22 +0800

    提交

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './assets/css/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props, context) {
+        super(props, context);
+    }
+    render() {
+        return (
+            <Fragment>
+                <div className="header">
+                    <input/><button>提交</button>
+                </div>
+                <ul>
+                    <li>学英语</li>
+                    <li>学react</li>
+                </ul>
+            </Fragment>
+        );
+    }
 }
+
 
 export default App;
